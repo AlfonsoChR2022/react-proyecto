4c012c42d4875a14f4fa92deb6a9f869a3434d53
AlfonsoChR2022 <alfonso.chavez.rgz@gmail.com>
2025-06-30 15:27:20 -0600

    Cambio 36

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Hola soy Alfonso CHAVEZ
+          Hola soy Alfonso Chávez Rodríguez
         </p>
         
         <a
